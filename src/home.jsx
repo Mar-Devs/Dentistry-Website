d32c336b8e2154a584eb2f919e9ced221493c745
Mar Devs <marah.mh50@gmail.com>
2026-06-01 20:09:30 +0300

    changed home bkg, fixed bugs

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,14 @@
 import "./home.css"
 import dentist from './assests/dentist.png'
 import calenderIcon from "./assests/calenderIcon.svg"
+import tooth3D from "./assests/3d-tooth.png"
 export function Home(){
     return(
         <>
         <section id="home">
             <div className="home-body">
                 <div className="home-body-left">
-                    <img src={dentist} alt="dentist-leaning-against-a-tooth" />
+                    <img src={tooth3D} alt="dentist-leaning-against-a-tooth" />
                 </div>
                 <div className="home-body-right">
                     <p dir="rtl">عياده متخصصه في مجال تعويض الاسنان الثابته و المتحركه</p>
