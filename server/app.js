fc6da832da831357d3c72e5b8d58dc28fdc6cdf4
Mar Devs <marah.mh50@gmail.com>
2026-07-08 15:03:23 +0300

    Added a server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import err from "node:console"
+import submitAppoitment from "./controllers/validators/contactFormValidator.js"
+
+const app = express();
+
+app.get("/trying", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/submission",submitAppoitment)
+
+const PORT = 5173
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw new err();
+  }
+
+  console.log(`Listening on port ${PORT}`)
+});
